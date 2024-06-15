25059e92038e438053b0a88ba23a5076290d0bc9
Volodymyr <babihuk888@gmail.com>
2024-06-15 12:40:27 +0300

    lesson 7 - filtered array

---
+++ js-7-arrays-methods/01-array-filtering/index.js
@@ -0,0 +1,11 @@
+// 1. Learn requirement (WHAT?)
+// 2. Create step by step algo (& input/output for functions) (HOW?) ++
+// 3. Write draft solution & testing - CODE
+// 4. Refactoring & final testing -> final solution
+
+// input: []
+// output: []
+
+const getSpecialNumbers = arr => arr.filter(el => el % 3 === 0);
+
+console.log(getSpecialNumbers([3, 56, 8, 90, 2, 6, 34]));
