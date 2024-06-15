2d1b3721167430749aa39325acc83b6680fbc30a
Volodymyr <babihuk888@gmail.com>
2024-06-15 15:30:55 +0300

    lesson 7 - task-6

---
+++ js-7-arrays-methods/06-array-clone/index.js
@@ -0,0 +1,7 @@
+// 1. Learn requirement (WHAT?)
+// 2. Create step by step algo (& input/output for functions) (HOW?) ++
+// 3. Write draft solution & testing - CODE
+// 4. Refactoring & final testing -> final solution
+
+const cloneArr = arr => (Array.isArray(arr) ? [...arr] : null);
+console.log(cloneArr([34, 6, 'test', 6, 0]));
