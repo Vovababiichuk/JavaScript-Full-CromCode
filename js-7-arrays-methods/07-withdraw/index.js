38d125493465cd1d0dd7f0b80bbc007ad67b4619
Volodymyr <babihuk888@gmail.com>
2024-06-15 20:13:19 +0300

    lesson 7 - task-7

--- js-7-arrays-methods/07-withdraw/index.js
+++ js-7-arrays-methods/07-withdraw/index.js
@@ -17,7 +17,7 @@ const withdraw = (clients, balances, client, amount) => {
 };
 
 // examples
-console.log(withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'Jhn', 50)); // ==> 37 (balances array should be [1400, 37, -6])
+console.log(withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'John', 50)); // ==> 37 (balances array should be [1400, 37, -6])
 console.log(withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'User', 10)); // ==> -1 (balances array should be [1400, 87, -6])
 
 /*
