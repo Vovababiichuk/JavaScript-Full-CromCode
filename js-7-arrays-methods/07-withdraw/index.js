14a71ee1d119c76033bdcdbb28140fb291b7d7e4
Volodymyr <babihuk888@gmail.com>
2024-06-15 19:29:52 +0300

    lesson 7 - task-12

--- js-7-arrays-methods/07-withdraw/index.js
+++ js-7-arrays-methods/07-withdraw/index.js
@@ -4,13 +4,16 @@
 // 4. Refactoring & final testing -> final solution
 
 const withdraw = (clients, balances, client, amount) => {
-  const index = clients.indexOf(client);
-  if (balances[index] > amount) {
-    return balances[index] - amount;
+  const clientIndex = clients.indexOf(client);
+
+  if (clientIndex === -1 || amount > balances[clientIndex]) {
+    return -1;
   }
-  return -1;
+
+  balances[clientIndex] -= amount;
+  return balances[clientIndex];
 };
 
 // examples
-console.log(withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'John', 50)); // ==> 37 (balances array should be [1400, 37, -6])
+console.log(withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'Jhn', 50)); // ==> 37 (balances array should be [1400, 37, -6])
 console.log(withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'User', 10)); // ==> -1 (balances array should be [1400, 87, -6])
