b6349ea8a747e08f89dbdf49365f0beba5e4fac2
Volodymyr <babihuk888@gmail.com>
2024-06-16 20:32:09 +0300

    js-9-task1

--- js-9-Objects-keys-val-enrt/01-methods-objects/index.js
+++ js-9-Objects-keys-val-enrt/01-methods-objects/index.js
@@ -1,11 +1,8 @@
-const getAdults = obj => {
-  const CHECK_AGE = 18;
-  console.log(
-    Object.entries(obj)
-      .filter(age => age[1] >= CHECK_AGE)
-      .map(name => name[0]),
-  );
-};
+const CHECK_AGE = 18;
+const getAdults = obj =>
+  Object.entries(obj)
+    .filter(age => age[1] >= CHECK_AGE)
+    .map(name => name[0]);
 
 console.log(getAdults({ 'John Doe': 19, Tom: 17, Bob: 18 }));
 console.log(getAdults({}));
