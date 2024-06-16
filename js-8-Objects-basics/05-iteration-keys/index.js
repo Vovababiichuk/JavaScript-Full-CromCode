d7f1e51311b5ee7ab7abc1718a2a20388e2bb33e
Volodymyr <babihuk888@gmail.com>
2024-06-16 16:45:24 +0300

    js-8-task5

---
+++ js-8-Objects-basics/05-iteration-keys/index.js
@@ -0,0 +1,17 @@
+// input: {};
+
+// function getKeys(obj) {
+//   return Object.keys(obj).join(', ');
+// }
+
+// console.log(getKeys({ name: 'John', age: 55, role: 'developer' }));
+
+//! ==========================
+
+// function getKeys(obj) {
+//   for (const key in obj) {
+//     console.log(key);
+//   }
+// }
+
+// getKeys({ name: 'John', age: 55, role: 'developer' });
