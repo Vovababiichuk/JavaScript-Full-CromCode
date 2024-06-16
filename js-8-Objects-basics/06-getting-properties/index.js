f7576af89f77eba4514115643f122c164da60c6d
Volodymyr <babihuk888@gmail.com>
2024-06-16 17:31:18 +0300

    js-8-task6

---
+++ js-8-Objects-basics/06-getting-properties/index.js
@@ -0,0 +1,38 @@
+// input: {}, []
+// output: {}
+
+//! ===============================================
+// const pickProps = (obj, props) => {
+//   const newObj = {};
+//   for (let key in obj) {
+//     props.forEach(el => {
+//       if (key === el) {
+//         return (newObj[key] = obj[key]);
+//       }
+//     });
+//   }
+//   return newObj;
+// };
+
+// // examples
+// console.log(pickProps({ a: 1, b: 2, c: 3 }, ['a', 'c'])); // ==> { a: 1, c: 3 }
+// console.log(pickProps({ a: 1, b: 2, c: 3 }, ['a', 'c', 'd', 'hex'])); // ==> { a: 1, c: 3 }
+// console.log(pickProps({}, ['a', 'c', 'd', 'hex']));
+
+//! ===============================================
+// const pickProps = (obj, props) => {
+//   const newObj = {};
+//   props.forEach(prop => {
+//     if (obj.hasOwnProperty(prop)) {
+//       newObj[prop] = obj[prop];
+//     }
+//   });
+//   return newObj;
+// };
+
+// // examples
+// console.log(pickProps({ a: 1, b: 2, c: 3 }, ['a', 'c'])); // ==> { a: 1, c: 3 }
+// console.log(pickProps({ a: 1, b: 2, c: 3 }, ['a', 'c', 'd', 'hex'])); // ==> { a: 1, c: 3 }
+// console.log(pickProps({}, ['a', 'c', 'd', 'hex'])); // ==> {}
+
+//! ===============================================
