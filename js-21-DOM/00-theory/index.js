547a1ae44cd842bb5deee1e055dc3d05b8b3ec84
Volodymyr <babihuk888@gmail.com>
2024-07-04 18:45:55 +0300

    todo

---
+++ js-21-DOM/00-theory/index.js
@@ -0,0 +1,15 @@
+const tasks = [
+	'Buy milk',
+	'Pick up Tom from airport',
+	'Visit party',
+	'Visit doctor',
+	'Buy meat',
+]
+
+const renderListItems = listItems => {
+	const listElem = document.querySelector('.list');
+
+	const listItemElem = document.createElement('li');  
+}
+
+renderListItems(tasks);
---
+++ js-21-DOM/01-to-do-render-list/index.js
@@ -0,0 +1,33 @@
+const tasks = [
+  { text: 'Buy milk', done: false },
+  { text: 'Pick up Tom from airport', done: false },
+  { text: 'Visit party', done: false },
+  { text: 'Visit doctor', done: true },
+  { text: 'Buy meat', done: true },
+];
+
+const renderTasks = tasksList => {
+  const list = document.querySelector('.list');
+
+  const listItemsElements = tasksList.map(({ text, done }) => {
+    const listItemElement = document.createElement('li');
+    const inputCheckBox = document.createElement('input');
+
+    listItemElement.classList.add('list__item');
+		if (done) {
+			listItemElement.classList.add('list__item_done')
+		}
+    inputCheckBox.classList.add('list__item-checkbox');
+
+    inputCheckBox.setAttribute('type', 'checkbox');
+		inputCheckBox.checked = done;
+
+    listItemElement.append(inputCheckBox, text);
+
+    return listItemElement;
+  });
+
+  list.append(...listItemsElements);
+};
+
+renderTasks(tasks);
