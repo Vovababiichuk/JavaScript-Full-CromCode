440053c8a56866113dd79db83b57d1a1833396e5
Volodymyr <babihuk888@gmail.com>
2024-07-05 10:08:08 +0300

    task9

---
+++ js-21-DOM/09-create-insert-elements2/index.js
@@ -0,0 +1,22 @@
+const finishList = () => {
+  const list = document.querySelector('.list');
+  const special = document.querySelector('.special');
+
+  const listItemLiOne = document.createElement('li');
+  listItemLiOne.textContent = '1';
+  const listItemLiFour = document.createElement('li');
+  listItemLiFour.textContent = '4';
+  const listItemLiSix = document.createElement('li');
+  listItemLiSix.textContent = '6';
+  const listItemLiEight = document.createElement('li');
+  listItemLiEight.textContent = '8';
+
+  list.prepend(listItemLiOne);
+  list.append(listItemLiEight);
+  special.prepend(listItemLiFour);
+  special.after(listItemLiSix);
+
+  console.log(list);
+};
+
+finishList();
