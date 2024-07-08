93907dd445813f8261161ae262755ff5fe402651
Volodymyr <babihuk888@gmail.com>
2024-07-08 20:37:50 +0300

    js-24 task1

--- js-24-new-Data/02-set-date/index.js
+++ js-24-new-Data/02-set-date/index.js
@@ -1,8 +1,8 @@
 const weekDays = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
+
 export const dayOfWeek = (date, days) => {
-	const day = new Date(date).getDate();
-	const dateInFuture = new Date(date).setDate(day + days);
-	return weekDays[new Date(dateInFuture).getDay()];
+    const dateInFuture = new Date(date.getTime() + days * 24 * 60 * 60 * 1000);
+    return weekDays[dateInFuture.getDay()];
 }
 
 console.log(dayOfWeek(new Date(2024, 0, 1), 14));
