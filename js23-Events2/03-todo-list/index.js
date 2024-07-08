7f3e52584c68efb4434e4b8104fa1b23440a7b7f
Volodymyr <babihuk888@gmail.com>
2024-07-08 17:32:57 +0300

    todo-list

--- js23-Events2/03-todo-list/index.js
+++ js23-Events2/03-todo-list/index.js
@@ -1,74 +1,70 @@
 const tasks = [
-  { id: '1', text: 'Buy milk', done: false },
-  { id: '2', text: 'Pick up Tom from airport', done: false },
-  { id: '3', text: 'Visit party', done: false },
-  { id: '4', text: 'Visit doctor', done: true },
-  { id: '5', text: 'Buy meat', done: true },
+  { text: 'Buy milk', done: false, id: '1' },
+  { text: 'Pick up Tom from airport', done: false, id: '2' },
+  { text: 'Visit party', done: false, id: '3' },
+  { text: 'Visit doctor', done: true, id: '4' },
+  { text: 'Buy meat', done: true, id: '5' },
 ];
 
 const listElem = document.querySelector('.list');
 
 const renderTasks = tasksList => {
   listElem.innerHTML = '';
-
-  tasksList
+  const tasksElems = tasksList
+    .slice()
     .sort((a, b) => a.done - b.done)
-    .forEach(({ text, done, id }) => {
+    .map(({ text, done, id }) => {
       const listItemElem = document.createElement('li');
       listItemElem.classList.add('list__item');
-
       const checkbox = document.createElement('input');
       checkbox.setAttribute('type', 'checkbox');
+      checkbox.setAttribute('data-id', id);
       checkbox.checked = done;
-      checkbox.dataset.id = id;
       checkbox.classList.add('list__item-checkbox');
-
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
       listItemElem.append(checkbox, text);
-      listElem.appendChild(listItemElem);
 
-      listItemElem.classList.toggle('list__item_done', done);
+      return listItemElem;
     });
+
+  listElem.append(...tasksElems);
 };
 
 renderTasks(tasks);
 
-// put your code here
+const onToggleTask = e => {
+  const isCheckbox = e.target.matches('.list__item-checkbox');
 
-const inputTask = document.querySelector('.task-input');
-const btnCreateTask = document.querySelector('.create-task-btn');
-
-const handleCreateTask = () => {
-  const taskText = inputTask.value.trim();
-  if (taskText) {
-    const newTask = {
-      id: `task${tasks.length + 1}`,
-      text: taskText,
-      done: false,
-    };
-    tasks.unshift(newTask);
-    renderTasks(tasks);
-    inputTask.value = '';
+  if (!isCheckbox) {
+    return;
   }
+
+  const taskData = tasks.find(task => task.id === e.target.dataset.id);
+  taskData.done = e.target.checked;
+  renderTasks(tasks);
 };
 
-btnCreateTask.addEventListener('click', handleCreateTask);
+listElem.addEventListener('click', onToggleTask);
 
-inputTask.addEventListener('keydown', e => {
-  if (e.key === 'Enter') {
-    handleCreateTask();
+const onCreateTask = () => {
+  const taskTitleInputElem = document.querySelector('.task-input');
+
+  const text = taskTitleInputElem.value;
+
+  if (!text) {
+    return;
   }
-});
+  taskTitleInputElem.value = '';
 
-
-const handleChangeTaskStatus = e => {
-  if (e.target.classList.contains('list__item-checkbox')) {
-    const taskId = e.target.dataset.id;
-    const taskIndex = tasks.findIndex(task => task.id === taskId);
-    if (taskIndex !== -1) {
-      tasks[taskIndex].done = !tasks[taskIndex].done;
-      renderTasks(tasks);
-    }
-  }
+  tasks.push({
+    text,
+    done: false,
+    id: Math.random().toString(),
+  });
+  renderTasks(tasks);
 };
 
-listElem.addEventListener('change', handleChangeTaskStatus);
+const createBtnElem = document.querySelector('.create-task-btn');
+createBtnElem.addEventListener('click', onCreateTask);
