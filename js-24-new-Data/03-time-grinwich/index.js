af3024d2cf0e37c7e64eccbd11ed14101b3f2836
Volodymyr <babihuk888@gmail.com>
2024-07-08 20:49:32 +0300

    js-24 task2

--- js-24-new-Data/03-time-grinwich/index.js
+++ js-24-new-Data/03-time-grinwich/index.js
@@ -1,5 +1,11 @@
 const getGreenwichTime = (date) => {
-	return date.toUTCString()
+	const formatter = new Intl.DateTimeFormat('en', {
+		timeZone: 'UTC',
+		hour: '2-digit',
+		minute: '2-digit',
+		hour12: false
+	})
+	return formatter.format(date)
 }
 
 getGreenwichTime(new Date())
