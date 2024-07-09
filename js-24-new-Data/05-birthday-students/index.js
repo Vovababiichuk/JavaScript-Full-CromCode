cbf71bb7c62a85973ca581598be8e4ff8bc4862d
Volodymyr <babihuk888@gmail.com>
2024-07-09 19:31:56 +0300

    task5

--- js-24-new-Data/05-birthday-students/index.js
+++ js-24-new-Data/05-birthday-students/index.js
@@ -1,34 +1,32 @@
-export const studentsBirthDays = students => {
-	const monthMap = students.reduce((acc, { name, birthDate }) => {
-		const date = new Date(birthDate)
-		const monthName = new Intl.DateTimeFormat('en', { month: 'short' }).format(
-			date
-		)
+export const studentsBirthDays = (students) => {
+  const monthMap = students.reduce((acc, { name, birthDate }) => {
+    const date = new Date(birthDate);
+    const monthName = new Intl.DateTimeFormat('en', { month: 'short' }).format(date);
 
-		if (!acc[monthName]) {
-			acc[monthName] = []
-		}
+    if (!acc[monthName]) {
+      acc[monthName] = [];
+    }
 
-		acc[monthName].push({ name, birthDate })
-		return acc
-	}, {})
+    acc[monthName].push({ name, birthDate });
+    return acc;
+  }, {});
 
-	for (const month in monthMap) {
-		monthMap[month].sort((a, b) => {
-			const year = new Date(a.birthDate).getFullYear()
-			const year2 = new Date(b.birthDate).getFullYear()
-			return year2 - year
-		})
-		monthMap[month] = monthMap[month].map(student => student.name)
-	}
+  for (const month in monthMap) {
+    monthMap[month].sort((a, b) => {
+      const dayA = new Date(a.birthDate).getDate();
+      const dayB = new Date(b.birthDate).getDate();
+      return dayA - dayB;
+    });
+    monthMap[month] = monthMap[month].map(student => student.name);
+  }
 
-	return monthMap
-}
+  return monthMap;
+};
 
 console.log(
-	studentsBirthDays([
-		{ name: 'Tom', birthDate: '01/15/2010' },
-		{ name: 'Ben', birthDate: '01/17/2008' },
-		{ name: 'Sam', birthDate: '03/15/2010' },
-	])
-)
+  studentsBirthDays([
+    { name: 'Tom', birthDate: '01/15/2010' },
+    { name: 'Ben', birthDate: '01/17/2008' },
+    { name: 'Sam', birthDate: '03/15/2010' },
+  ])
+);
