8d52acb6f940944c0c7e14f90a429e67daf0a86b
Volodymyr <babihuk888@gmail.com>
2024-07-09 16:18:41 +0300

    task5

--- js-24-new-Data/05-birthday-students/index.js
+++ js-24-new-Data/05-birthday-students/index.js
@@ -1,16 +1,24 @@
 export const studentsBirthDays = students => {
-  return students.sort((a, b) => {
-    const dayOfWeekA = new Date(a.birthDate).getDay();
-    const dayOfWeekB = new Date(b.birthDate).getDay();
-    return dayOfWeekB - dayOfWeekA;
-  });
-};
+	return students.reduce((acc, { name, birthDate }) => {
+		const date = new Date(birthDate)
+		const monthName = new Intl.DateTimeFormat('en', { month: 'short' }).format(
+			date
+		)
+
+		if (!acc[monthName]) {
+			acc[monthName] = []
+		}
+
+		acc[monthName].push(name)
+		return acc
+	}, {})
+}
 
 console.log(
-  studentsBirthDays([
-    { name: 'Den', birthDate: '01/15/2010' },
-    { name: 'John', birthDate: '10/10/2010' },
-    { name: 'Ann', birthDate: '12/12/2010' },
-    { name: 'Mike', birthDate: '01/01/2010' },
-  ]),
-);
+	studentsBirthDays([
+		{ name: 'Den', birthDate: '01/15/2010' },
+		{ name: 'John', birthDate: '10/10/2010' },
+		{ name: 'Ann', birthDate: '12/12/2010' },
+		{ name: 'Mike', birthDate: '01/01/2010' },
+	])
+)
