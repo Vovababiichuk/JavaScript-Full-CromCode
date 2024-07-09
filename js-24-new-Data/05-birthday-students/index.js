1a50306ddd82ad4026e48b42b9132fcf0453d855
Volodymyr <babihuk888@gmail.com>
2024-07-09 19:10:32 +0300

    task5

--- js-24-new-Data/05-birthday-students/index.js
+++ js-24-new-Data/05-birthday-students/index.js
@@ -1,25 +1,29 @@
 export const studentsBirthDays = students => {
-	return students.reduce((acc, { name, birthDate }) => {
-		const date = new Date(birthDate)
-		const monthName = new Intl.DateTimeFormat('en', { month: 'short' }).format(
-			date
-		)
+  const monthMap = students.reduce((acc, { name, birthDate }) => {
+    const date = new Date(birthDate);
+    const monthName = new Intl.DateTimeFormat('en', { month: 'short' }).format(date);
 
-		if (!acc[monthName]) {
-			acc[monthName] = []
-		}
+    if (!acc[monthName]) {
+      acc[monthName] = [];
+    }
 
-		acc[monthName].push(name)
-		return acc
-	}, {})
-}
+    acc[monthName].push({ name, birthDate });
+    return acc;
+  }, {});
 
+  for (const month in monthMap) {
+    monthMap[month].sort((a, b) => new Date(a.birthDate) - new Date(b.birthDate));
+    monthMap[month] = monthMap[month].map(student => student.name);
+  }
+
+  return monthMap;
+};
 
 console.log(
-	studentsBirthDays([
-		{ name: 'Den', birthDate: '01/15/2010' },
-		{ name: 'John', birthDate: '10/10/2010' },
-		{ name: 'Ann', birthDate: '12/12/2010' },
-		{ name: 'Mike', birthDate: '01/01/2010' },
-	])
-)
+  studentsBirthDays([
+    { name: 'Den', birthDate: '01/15/2010' },
+    { name: 'John', birthDate: '10/10/2010' },
+    { name: 'Ann', birthDate: '12/12/2010' },
+    { name: 'Mike', birthDate: '01/01/2010' },
+  ]),
+);
