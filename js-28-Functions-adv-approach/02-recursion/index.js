516b181eb76a0143d761f3f616991230d8e08e32
Volodymyr <babihuk888@gmail.com>
2024-07-12 19:02:58 +0300

    task2

---
+++ js-28-Functions-adv-approach/02-recursion/index.js
@@ -0,0 +1,36 @@
+const favorites = ['id-2'];
+
+const tree = {
+  id: 'id-1',
+  name: 'Products',
+  nodes: [
+    {
+      id: 'id-2',
+      name: 'Food',
+      nodes: [
+        {
+          id: 'id-6',
+          name: 'Drinks',
+          nodes: [],
+        },
+      ],
+    },
+    {
+      id: 'id-17',
+      name: 'Vehicles',
+      nodes: [],
+    },
+  ],
+};
+
+const markFavorites = (tree, favorites) => {
+  const isFavorites = favorites.includes(tree.id);
+
+  return {
+    ...tree,
+    isFavorites,
+    nodes: tree.nodes.map(childNode => markFavorites(childNode, favorites)),
+  };
+};
+
+console.log(markFavorites(tree, favorites));
