0369781c9164f50b026af19b82b7a4f140aa518d
Volodymyr <babihuk888@gmail.com>
2024-07-15 18:41:08 +0300

    task1-js29

--- js-29-Asynchronous-code/02-load-image/index.js
+++ js-29-Asynchronous-code/02-load-image/index.js
@@ -1,4 +1,4 @@
-const addImage = (imgSrc, callback) => {
+export const addImage = (imgSrc, callback) => {
   const page = document.querySelector('.page');
   const createImageEl = document.createElement('img');
   createImageEl.setAttribute('alt', 'My Image');
