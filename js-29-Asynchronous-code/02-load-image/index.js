ffcbf981e65d1a4054513dfddb31dfd8d3a7e7a5
Volodymyr <babihuk888@gmail.com>
2024-07-15 19:26:07 +0300

    task1-js29

--- js-29-Asynchronous-code/02-load-image/index.js
+++ js-29-Asynchronous-code/02-load-image/index.js
@@ -1,45 +1,22 @@
-// const buttonElem = document.querySelector("#button");
-
-// buttonElem.addEventListener('click', () => console.log("click"));
-
-// let sum = 0;
-// const startTime = new Date();
-// console.log(`Cycle started`);
-// for (let i = 0; i < 5e9; i++) {
-//   sum += i;
-// }
-// const endTime = new Date();
-// console.log(`Cycle took ${endTime - startTime} ms`);
-
-// JavaScript однопоточний код
-// одна операція в одну одиницю часу
-
-// --------------------------------
-// --------------------------------
-// ----------- addImage -----------
-// --------------------------------
-// --------------------------------
-
-const addImage = (imgSrc, callback) => {
+export const addImage = (imgSrc, callback) => {
   const imgElem = document.createElement('img');
-  imgElem.setAttribute('alt', 'My photo');
+  imgElem.setAttribute('alt', 'My Image');
   imgElem.src = imgSrc;
+  const page = document.querySelector('.page');
+  page.append(imgElem);
 
-  const containerElem = document.querySelector('.page');
-  containerElem.append(imgElem);
+  const onImageError = () => {
+    callback(new Error('Image load is failed!'));
+  };
 
   const onImageLoaded = () => {
     const { width, height } = imgElem;
     callback(null, { width, height });
   };
 
+  imgElem.addEventListener('error', onImageError);
   imgElem.addEventListener('load', onImageLoaded);
-
-  imgElem.addEventListener('error', () => callback('Image load failed'));
 };
-
-const imgSrc =
-  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
 
 const onImageLoaded = (error, imgElem) => {
   if (error) {
@@ -49,7 +26,11 @@ const onImageLoaded = (error, imgElem) => {
 
   const { width, height } = imgElem;
   const sizeElem = document.querySelector('.image-size');
+
   sizeElem.textContent = `${width} x ${height}`;
 };
 
-addImage(imgSrc, onImageLoaded);
+addImage(
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+  onImageLoaded,
+);
