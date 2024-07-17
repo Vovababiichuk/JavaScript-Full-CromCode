419f3b5e2fdfc78eebeb9f269191b9716af3bc85
Volodymyr <babihuk888@gmail.com>
2024-07-17 20:18:50 +0300

    task2

--- js-29-Asynchronous-code/03-write-with-interval/index.js
+++ js-29-Asynchronous-code/03-write-with-interval/index.js
@@ -6,7 +6,7 @@ export const pinger = (count, period) => {
     console.log('Ping');
     currentCount += 1;
 
-    if (currentCount >= count) {
+    if (currentCount >= count - 1) {
       clearInterval(intervalId);
     }
   }, period);
@@ -15,4 +15,4 @@ export const pinger = (count, period) => {
 
 // examples
 pinger(5, 100); // makes 5 writes with 100 ms interval
-pinger(7, 150); // makes 7 writes with 1500 ms interval
+// pinger(7, 150); // makes 7 writes with 1500 ms interval
