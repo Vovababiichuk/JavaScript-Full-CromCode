6c317357b0ed0fac2c9bddb82ebfc5897b22442f
Volodymyr <babihuk888@gmail.com>
2024-07-17 21:00:04 +0300

    task5

---
+++ js-29-Asynchronous-code/06-Load-user-data/index.js
@@ -0,0 +1,24 @@
+const requestUserData = (userId, callback) => {
+  const randomDelay = Math.random() * 1000;
+
+  setTimeout(() => {
+    if (userId === 'broken') {
+      callback(userId, 'Failed to load user data');
+    } else {
+      callback(null, {
+        userId,
+        email: `${userId}@example.com`,
+      });
+    }
+  }, randomDelay);
+};
+
+const onUserDataLoaded = (error, data) => {
+  if (error) {
+    return console.log(error);
+  }
+  return console.log(data);
+};
+
+requestUserData('1', onUserDataLoaded); // Should return { userId: '1', email: '1@example.com' }
+requestUserData('broken', onUserDataLoaded); // Should return 'Failed to load user data'
