a7cb0768ad13be231dcbeca52895d4bfc78fed84
Volodymyr <babihuk888@gmail.com>
2024-08-01 12:50:32 +0300

    grud

--- js-34-Comolex-practice-HTTP/GRUD/index.js
+++ js-34-Comolex-practice-HTTP/GRUD/index.js
@@ -11,19 +11,13 @@ export function getUsersList() {
     .catch(err => {
       console.error('There was an error with fetching users', err);
     })
-		// .then(users => console.log(users));
 }
-
-getUsersList().then(users => console.log(users));
-
 
 export function getUserById(userId) {
 	return fetch(`${baseUrl}/${userId}`)
 		.then(res => res.json())
 		.catch(err => console.error(err))
-		.then(user => console.log(user));
 }
-
 
 export function createUser(userData) {
 	return fetch(baseUrl, {
@@ -35,17 +29,13 @@ export function createUser(userData) {
 	})
 		.then(res => res.json())
 		.catch(err => console.error(err))
-		.then(user => console.log(user));
 }
-
-
 
 export function deleteUser(userId) {
 	return fetch(`${baseUrl}/${userId}`, {
 		method: 'DELETE',
 	})
 	.catch(err => console.error(err))
-	.then(() => console.log(`User deleted with id ${userId}`));
 }
 
 
@@ -66,37 +56,37 @@ export function updateUser(userId, userData) {
 	})
 	.catch(err => console.error('There was an error with updating user', err))
 }
-// // examples
-// getUsersList().then(users => {
-//   console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
-// });
 
-// getUserById('2').then(userData => {
-//   console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
-// });
+getUsersList().then(users => {
+  console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
+});
 
-// const newUserData = {
-//   email: 'cool@email.com',
-//   firstName: 'Iron',
-//   lastName: 'Man',
-//   age: 42,
-// };
+getUserById('2').then(userData => {
+  console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
+});
 
-// createUser(newUserData).then(() => {
-//   console.log('User created');
-// });
+const newUserData = {
+  email: 'cool@email.com',
+  firstName: 'Iron',
+  lastName: 'Man',
+  age: 42,
+};
 
-// const updatedUserData = {
-//   email: 'new@email.com',
-//   firstName: 'John',
-//   lastName: 'Doe',
-//   age: 17,
-// };
+createUser(newUserData).then(() => {
+  console.log('User created');
+});
 
-// updateUser('1', updatedUserData).then(() => {
-//   console.log('User updated');
-// });
+const updatedUserData = {
+  email: 'new@email.com',
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 17,
+};
 
-// deleteUser('2').then(() => {
-//   console.log('User updated');
-// });
+updateUser('1', updatedUserData).then(() => {
+  console.log('User updated');
+});
+
+deleteUser('2').then(() => {
+  console.log('User updated');
+});
