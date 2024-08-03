aba817bf3c301f313e3bfba73506b9789bca3ea4
Volodymyr <babihuk888@gmail.com>
2024-08-03 17:14:34 +0300

    task1

--- js-36-Promise-async-await/02-Get-user/index.js
+++ js-36-Promise-async-await/02-Get-user/index.js
@@ -1,6 +1,6 @@
 export const fetchUser = async userId => {
   try {
-    const response = await fetch(`https://api.github.com/users/${uerId}`);
+    const response = await fetch(`https://api.github.com/users/${userId}`);
     if (!response.ok) {
       return null;
     }
