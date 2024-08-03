f052303130b50f2ffe441fba8b9e603c9554dbfa
Volodymyr <babihuk888@gmail.com>
2024-08-03 17:58:33 +0300

    task4

--- js-36-Promise-async-await/04-User-blogs/index.js
+++ js-36-Promise-async-await/04-User-blogs/index.js
@@ -1,14 +1,28 @@
-export const getUsersBlogs = users => {
-  return new Promise((resolve, reject) => {
-    if (users.length === 0) {
-      resolve([]);
-    } else {
-      const linksList = users.map(user => `https://opensource.${user}.org`);
-      resolve(linksList);
-    }
-  });
+const baseUrl = 'https://api.github.com/users';
+
+export const getUsersBlogs = async users => {
+  try {
+    const res = await Promise.all(users.map(user => fetch(`${baseUrl}/${user}`)));
+
+    const userData = await Promise.all(res.map(res => {
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+      return res.json();
+    }));
+
+    return userData.map(user => user.blog);
+  } catch (err) {
+    console.error('Failed to get user data:', err.message);
+    return null;
+  }
 };
 
-// examples
-getUsersBlogs(['google', 'facebook', 'reactjs']).then(linksList => console.log(linksList)); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
-getUsersBlogs(['microsoft']).then(linksList => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
+// Examples
+getUsersBlogs(['google', 'facebook', 'reactjs'])
+  .then(linksList => console.log(linksList)) // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
+  .catch(err => console.error(err));
+
+getUsersBlogs(['microsoft'])
+  .then(linksList => console.log(linksList)) // ==> ["https://opensource.microsoft.com"]
+  .catch(err => console.error(err));
